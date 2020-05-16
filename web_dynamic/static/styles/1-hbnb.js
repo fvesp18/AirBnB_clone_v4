b65105ba6f24bf58e619a9703d8ef2e854310b01
fvesp18 <1081@holbertonschool.com>
2020-05-16 00:26:00 +0000

    [UPDATE] Task 2 jQuery, if checkbox checked store var id else remove var id

---
+++ web_dynamic/static/styles/1-hbnb.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// Listens for changes on each input checkbox
+$(document).ready(function () {
+	if ($(input.checkbox).is(':checked') {
+		let stored_id = "something goes here";
+	}) else {
+		stored_id.last().remove();
+	}
+})
