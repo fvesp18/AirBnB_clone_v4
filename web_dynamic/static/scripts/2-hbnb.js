2f0fa54b82c7bb662d380eb8d2a1c85513ba2569
fvesp18 <1081@holbertonschool.com>
2020-05-19 04:55:53 +0000

    [UPDATE] Task 3, make changes

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+window.onload = function () {
+	const selected = {};
+	$('INPUT:checkbox').change(function () {
+	  if ($(this).is(':checked')) {
+		selected[$(this).attr('data-id')] = $(this).attr('data-name');
+	  } else {
+		delete selected[($(this).attr('data-id'))];
+	  }
+	  const amenlist = $.map(selected, function (v, k) {
+		return v;
+	  });
+	  let output = '';
+	  for (let i = 0; i < amenlist.length; i++) {
+		output += amenlist[i];
+		if (i < amenlist.length - 1) {
+		  output += ', ';
+		}
+	  }
+	  output += '\xa0';
+	  $('DIV.amenities h4').html(output);
+	});
+	$.get('http://0.0.0.0:5001/api/v1/status/', function(data) {
+			if (data.statusCode === 200) {
+				// add something to somethign
+			} else {
+				// remove somethiugn from somehting
+			}
+  };
